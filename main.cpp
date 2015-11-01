24ca675bac30fcb2483eb74636750d9da7ec77af
cassisnaro <narowirf@gmail.com>
2015-11-01 15:29:49 +0100

    CMakeLists created, and stringstream used for table's row

--- main.cpp
+++ main.cpp
@@ -6,16 +6,16 @@
 #include <vector>
 #include <locale>
 #include <map>
-
-#include "main.h"
+#include <sstream>
 
 using namespace std;
 
-int convert_2d_index(int i, int j, int N);
+string print_table_1_row(string language, unsigned long N, unsigned long E);
+long convert_2d_index(unsigned long i, unsigned long j, unsigned long N);
 
 int main(int argc, char **argv){
-    long N;
-    long num_edges;
+    unsigned long N;
+    unsigned long num_edges;
 
     std::locale::global(std::locale(""));
     std::wifstream wf("./data/Arabic_syntactic_dependency_network.txt"); // test.txt contains utf-8 text
@@ -23,17 +23,17 @@ int main(int argc, char **argv){
 
     wf >> N >> num_edges;
 
-    long size = (N-1)*N/2;
+    unsigned long size = (N-1)*N/2;
     vector<bool> matrix;
     cerr << "size:"<<size<<endl;
     matrix.resize(size, false);
 
 
-    int count=0;
+    unsigned long count=0;
     map<wstring, int> conversion_map;
     for(int i=0; i<num_edges; i++ ){
         wf >> readed_string_1 >> readed_string_2;
-        int id1, id2;
+        unsigned long id1, id2;
         auto conversion_map_it_1 = conversion_map.find(readed_string_1);
         if (conversion_map_it_1==conversion_map.end()){
             conversion_map.insert(std::pair<wstring,int>(readed_string_1, count));
@@ -52,12 +52,14 @@ int main(int argc, char **argv){
             id2 = conversion_map_it_2->second;
         }
         if (id1 != id2){
-            int index_lineal = convert_2d_index(id1, id2, N);
+            long index_lineal = convert_2d_index(id1, id2, N);
             matrix[index_lineal] = true;
         }
     }
     wcout << readed_string_1 << " " << readed_string_2 << endl;
-    cout << count << endl;
+    wcout << count << endl;
+
+    print_table_1_row("arabic", N, num_edges);
 
     /*cin >> N >> num_edges;
     for (int i=0; i<num_edges; i++){
@@ -67,12 +69,18 @@ int main(int argc, char **argv){
     return 0;
 }
 
-int convert_2d_index(int i, int j, int N){
+string print_table_1_row(string language, unsigned long N, unsigned long E){
+    stringstream returnStringStream;
+    returnStringStream << language<< "& "<<N<<"& "<<E<<"& "<<E/(double)N<<"& "<<2*(double)E/(N*(N+1));
+    return returnStringStream.str();
+}
+
+long convert_2d_index(unsigned long i, unsigned long j, unsigned long N){
     if(i > j){
-        int tmp = i;
+        unsigned long tmp = i;
         i = j;
         j = tmp;
     }
-    int index_complete_matrix_1D = i*N - i*(i+1)/2 + j-(i+1);
+    long index_complete_matrix_1D = i*N - i*(i+1)/2 + j-(i+1);
     return index_complete_matrix_1D;
 }
