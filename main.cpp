343dba63c72bbde44fb082125de4ef673a489f67
cassisnaro <narowirf@gmail.com>
2015-11-11 17:07:26 +0100

    Cleaned up Main.

--- main.cpp
+++ main.cpp
@@ -12,22 +12,8 @@ using namespace std;
 
 int main(int argc, char *argv[]){
 
-    //srand(1234);
     int seed = 1234;
     std::default_random_engine generator(seed);
-
-
-    //std::vector<SyntacticDependencyTree*> trees;
-    /*trees.push_back(new SyntacticDependencyTree("Arabic","./data/Arabic_syntactic_dependency_network.txt"));
-    trees.push_back(new SyntacticDependencyTree("Basque","./data/Basque_syntactic_dependency_network.txt"));
-    trees.push_back(new SyntacticDependencyTree("Catalan","./data/Catalan_syntactic_dependency_network.txt"));
-    trees.push_back(new SyntacticDependencyTree("Chinese","./data/Chinese_syntactic_dependency_network.txt"));
-    trees.push_back(new SyntacticDependencyTree("Czech", "./data/Czech_syntactic_dependency_network.txt"));
-    trees.push_back(new SyntacticDependencyTree("English","./data/English_syntactic_dependency_network.txt"));
-    trees.push_back(new SyntacticDependencyTree("Greek","./data/Greek_syntactic_dependency_network.txt"));
-    trees.push_back(new SyntacticDependencyTree("Hungarian","./data/Hungarian_syntactic_dependency_network.txt"));
-    trees.push_back(new SyntacticDependencyTree("Italian","./data/Italian_syntactic_dependency_network.txt"));
-    trees.push_back(new SyntacticDependencyTree("Turkish","./data/Turkish_syntactic_dependency_network.txt"));*/
 
     std::vector<string> langs; std::vector<string> paths;
     langs.push_back("Catalan");paths.push_back("./data/Catalan_syntactic_dependency_network.txt");
@@ -35,6 +21,7 @@ int main(int argc, char *argv[]){
     langs.push_back("Czech");paths.push_back("./data/Czech_syntactic_dependency_network.txt");
     langs.push_back("English");paths.push_back("./data/English_syntactic_dependency_network.txt");
     langs.push_back("Hungarian");paths.push_back("./data/Hungarian_syntactic_dependency_network.txt");
+
     std::vector<double> Cs;
     Cs.push_back(0.341010236465873);
     Cs.push_back(0.326123380880794);
@@ -83,39 +70,6 @@ int main(int argc, char *argv[]){
         cerr << "original deleted" << endl;
         cerr << "================================" << endl << endl;
     }
-
-
-
-    //SyntacticDependencyTree * ER = SyntacticDependencyTree::ErdosRenyi(language.getNNodes(),language.getNEdges(),generator);
-
-    /*for(int i=0; i<4; i++){
-        ER->closenessCentralityGEQ(C);
-    }
-    cout << float(clock()-begin_time)/ CLOCKS_PER_SEC;
-    ER->sortDecreasing();
-    begin_time = clock();
-    for(int i=0; i<4; i++){
-        ER->closenessCentralityGEQ(C);
-    }
-    cout << float(clock()-begin_time)/ CLOCKS_PER_SEC;*/
-
-    /*for(int i=0; i<20; i++){
-        SyntacticDependencyTree * ER = SyntacticDependencyTree::ErdosRenyi(language.getNNodes(),language.getNEdges(),generator);
-        ER->sortDecreasing();
-        b = ER->closenessCentralityGEQ(C);
-        cout << "Cer " << (b ? ">=" : "<") << " C" << endl;
-        delete ER;
-    }
-
-
-    for(int i=0; i<20; i++) {
-        SyntacticDependencyTree *SM = SyntacticDependencyTree::SwitchingModel(language, log(language.getNEdges()),
-                                                                              generator);
-        SM->sortDecreasing();
-        //b = SM->closenessCentralityGEQ(C);
-        //cout << "Csm " << (b ? ">=" : "<") << " C" << endl;
-        delete SM;
-    }*/
 
     return 0;
 }
